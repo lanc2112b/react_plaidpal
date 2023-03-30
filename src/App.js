83d0ee4a3992a7166b40c7a3494e964d79bae9f3
Tony Lancaster <lanc1968@gmail.com>
2023-03-30 16:25:10 +0100

    Add dashboard file. TODO: Refactor dashboard.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Login from './components/Login';
 import Landing from './components/Landing';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
         <Routes>
         <Route path="/" element={<MainSection element={<Landing />} />} />
         
-          <Route path="/login" element={<MainSection element={<Login />} />} />
+        <Route path="/login" element={<MainSection element={<Login />} />} />
+        <Route path="/dashboard" element={<MainSection element={<Dashboard />} />} />
         
         </Routes>
       <Footer />
--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -3,16 +3,18 @@ import { Button } from 'react-bootstrap';
 const LoginButton = () => { 
 
  const handleLogin = () => {
-    const backendURL = `http://localhost:8000`;
-    window.location.href = `${backendURL}/auth/google`;
+     const backendURL = `https://plaidpal-api.onrender.com`;
+     window.location.href = `${backendURL}/auth/google`;
+     //window.location.href = `${backendURL}/api/users`;
   };
 
     return (
-
+        <>
         <Button variant="danger" onClick={handleLogin}>
             <i className="fas fa-user me-2"></i>
             Login w/google
         </Button>
+        </>
 
     )
 }
