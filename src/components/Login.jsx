63d75b385f041654b3bea14baea56b6a72b5bccc
Tony Lancaster <lanc1968@gmail.com>
2023-04-03 19:48:06 +0100

    Alter text in login & signup page.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,19 +4,23 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 const Login = () => {
 
+    //const theUser = localStorage.getItem("user");
+
+    //console.log(theUser);
+
     return (
 
         <section className="Login">
             <Container className="mt-sm-3 mt-lg-5 ">
                 <Row>
                     <Col></Col>
-                    <Col xs={12} md={10} lg={8} className="p-5 shadow-sm d-flex flex-column justify-content-center align-items-center  bg-white">
+                    <Col xs={12} md={10} lg={8} className="p-5 shadow-sm d-flex flex-column justify-content-center align-items-center text-center  bg-white">
 
-                        <h1>Login</h1>
-                        <p>Welcome back: var from user storage</p>
-                        <p>
-                            <span>Some login text here if needed
-                            </span>
+                        <h3 className="display-6">Login</h3>
+                        <p>Welcome Back</p>
+                        <p className="mb-5">
+                            Please login below to access your accounts.
+                            
                         </p>
                         <FlashMessage />
                         <LoginButton />
--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -10,27 +10,39 @@ const RegisterPage = () => {
                 <Container className="mt-sm-3 mt-lg-5">
                 <Row>
                     <Col></Col>
-                    <Col xs={12} md={10} lg={8} className="p-5 shadow-sm d-flex flex-column justify-content-center align-items-center bg-white">
+                    <Col xs={12} md={10} lg={6} className="p-5 shadow-sm d-flex flex-column justify-content-center align-items-center text-justify bg-white">
                         
-                        <h1>Register</h1>
-                        <p>Register now to get the latest: </p>
+                        <h2 className="mb-5">Sign Up</h2>
+                          
                         <p>
-                            <span>Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Amet culpa expedita non quis, impedit esse fugit,
-                            quibusdam atque velit facere at nam exercitationem ex cum
-                                temporibus, iusto voluptatum repellat cupiditate.
-                            </span>
-                            <span>Sapiente, tempora exercitationem, tenetur est
-                                consequatur velit rerum adipisci temporibus quos,
-                                debitis praesentium pariatur in illo? Tenetur ut
-                                expedita deleniti voluptatum consequuntur, debitis
-                                exercitationem quibusdam iste sapiente repellat ipsam
-                                id!
-                            </span>
-                           
+                            By signing up you'll be able to:
                         </p>
+                        <p>
+                            View all of your transactions
+                            in one place.
+                        </p>
+                        <p>Monitor expenditure and income.</p>
+                        <p>Visualise data by type, such as Eating out, Groceries and many more.</p>
+                        <p>Filter transactions by, date, account, income, expenditure and more.</p>
+
+
+
+
+                        <p>
+                                Note: This application requires the following from Google:
+                            </p>
+
+                            <ul className="list-unstyled">
+                                <li>Your name</li>
+                                <li>Email address</li>
+                                <li>Profile picture</li>
+                            </ul>
+                            
+   
+
+                        
                         <FlashMessage />
-                        <RegisterButton />
+                        <RegisterButton/>
                     </Col>
                     <Col></Col>
                     </Row>
