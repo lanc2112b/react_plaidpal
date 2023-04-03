411b9183671d346fbf7350b23d489112ef1d76f7
Tony Lancaster <lanc1968@gmail.com>
2023-04-03 17:45:08 +0100

    404 page added.

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -2,13 +2,20 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 
 const Error404 = () => {
 
+    const navigateHome = () => {
+
+        window.location.replace('/');
+    }
+
     return (
         <>
-            <section className="server-error">
-                <Container className="">
+            <section className="server-error d-flex align-items-end pb-4">
+                <Container>
                     <Row>
-                        <Col className="col col-12 d-flex justify-content-center align-items-center flex-fill">
-                            <Button variant="danger" onClick={() => console.log("Danger")}>
+                        <Col className="col col-10 offset-1 d-flex flex-column justify-content-center align-items-center p-4 error-page-message text-white text-center">
+                            <h4 className="display-5"> Page Not Found </h4>
+                            <p>Check the requested page and try again, alternatively use the nav bar above.</p>
+                            <Button variant="info" onClick={navigateHome}>
                                 <i className="fa-solid fa-left-long me-2"></i>
                             Go Back
                         </Button>
