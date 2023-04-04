7fb02ed21bc0e616aded2440c7fec0c823159819
HaleemHussain <haleemhussain64@gmail.com>
2023-04-04 15:38:22 +0100

    fixed an issue

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -4,6 +4,8 @@ import {UserContext} from "../contexts/User";
 import {getTransactions} from "../api/api";
 import LoaderSmall from "./LoaderSmall";
 import Donut from "./Donut";
+import {Bar} from "react-chartjs-2";
+import Barchart from "./Barchart";
 
 
 const Summary = () => {
@@ -30,7 +32,7 @@ const Summary = () => {
            
             {/** component for visualisation here */}
             <Donut list={list} />
-
+            <Barchart list={list}/>
 
             {/** component for transaction list here */}
             <SummaryTransactionsList list={list} />
