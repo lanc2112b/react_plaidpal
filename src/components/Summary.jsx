5d11f10b8fc575f6ba2a1285b711084f4806344a
IzaanD98 <izzy.dalvi@gmail.com>
2023-04-04 12:11:47 +0100

    added filter functionality to summaryFilter

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,42 +1,39 @@
 import SummaryTransactionsList from "./SummaryTransactionsList";
-import {useContext, useEffect, useState} from "react";
-import {UserContext} from "../contexts/User";
-import {getTransactions} from "../api/api";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../contexts/User";
+import { getTransactions } from "../api/api";
 import LoaderSmall from "./LoaderSmall";
 import Donut from "./Donut";
 import SummaryFilter from "./SummaryFilter";
 
-
 const Summary = () => {
-    const { user } = useContext(UserContext);
-    const [list, setList] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const { user } = useContext(UserContext);
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        setLoading(true);
-        getTransactions(user.googleId)
-            .then((results) => {
-                setList(results);
-            }).catch((error) => {
-        });
-        setLoading(false);
+  useEffect(() => {
+    setLoading(true);
+    getTransactions(user.googleId)
+      .then((results) => {
+        setList(results);
+      })
+      .catch((error) => {});
+    setLoading(false);
+  }, [user.googleId]);
 
-    }, [user.googleId]);
+  if (loading) return <LoaderSmall content={"Loading transactions..."} />;
 
-    if (loading) return <LoaderSmall content={"Loading transactions..."} />;
-    
-    console.log(list, 'summary 27')
-    return (
-        <>
-           
-            {/** component for visualisation here */}
-            <Donut list={list} />
+  console.log(list, "summary 27");
+  return (
+    <>
+      {/** component for visualisation here */}
+      <Donut list={list} />
 
-            <SummaryFilter />
-            {/** component for transaction list here */}
-            <SummaryTransactionsList list={list} />
-        </>
-    )
-}
+      <SummaryFilter setList={setList} />
+      {/** component for transaction list here */}
+      <SummaryTransactionsList list={list} />
+    </>
+  );
+};
 
 export default Summary;
