51bb08bbbc828119952c4b2b8c21b13095de0c9b
Tony Lancaster <lanc1968@gmail.com>
2023-04-05 08:54:55 +0100

    Replace Lorem.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Landing = () => {
 
     return (<section className="home">
         <img src="/chase_bank_1x.png" alt="" className="landing-image mb-3" />
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos minima vero, aliquam esse, voluptatibus atque maxime consectetur, quisquam quia obcaecati a veniam sequi enim necessitatibus fugit nulla. Exercitationem, magni voluptatibus.</p>
+        <p>
+            Welcome to PlaidPal, the ultimate financial management tool designed to help you manage your finances and reach your financial goals.
+            <br />With PlaidPal, you can easily connect all of your bank accounts and credit cards in one place, and get a comprehensive view of your
+            financial transactions and balances. <strong><Link to="/signup">Sign up for PlaidPal today and take control of your financial life!</Link></strong></p>
     </section>)
 
 }
