ceef0f43f2f4fc693ca2d1353fec07ea0f2c990e
Tony Lancaster <lanc1968@gmail.com>
2023-05-01 16:07:38 +0100

    Comment breakpoint display.

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -97,14 +97,14 @@ const HeaderNav = () => {
       </Navbar.Collapse>
     </Container>
       </Navbar>
-      <div className="position-fixed sticky-top text-nowrap rounded-3 small p-1 m-3 mt-5 bg-info opacity-25">
+      {/* <div className="position-fixed sticky-top text-nowrap rounded-3 small p-1 m-3 mt-5 bg-info opacity-25">
         <div className="d-block d-sm-none">Extra Small (xs)</div>
         <div className="d-none d-sm-block d-md-none">Small (sm)</div>
         <div className="d-none d-md-block d-lg-none">Medium (md)</div>
         <div className="d-none d-lg-block d-xl-none">Large (lg)</div>
         <div className="d-none d-xl-block d-xxl-none">X-Large (xl)</div>
         <div className="d-none d-xxl-block">XX-Large (xxl)</div>
-      </div>
+      </div> */}
     </>)
 }
 
